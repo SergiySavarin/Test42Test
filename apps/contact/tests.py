7f3780b96fb32e341d1c06ff2bc15e310ef9ffe1
SergiySavarin <vir.host@gmail.com>
2015-12-25 10:50:05 +0200

    ticket:1 add test for view, add view code

--- apps/contact/tests.py
+++ apps/contact/tests.py
@@ -1,3 +1,18 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-# Create your tests here.
+from views import contact
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, contact)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = contact(request)
+        expected_html = render_to_string('contact.html')
+        self.assertEqual(response.content.decode(), expected_html)
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,18 +1,4 @@
 from django.shortcuts import render
 
 def contact(request):
-    owner = {
-        'name': 'Sergiy',
-        'last_name': 'Savarin',
-        'birth_date': '30/11/1986',
-        'email': 'vir.host@gmail.com',
-        'jabber': 'sergiysavarin@khavr.com',
-        'skype': 'sergiy_savarin',
-        'facebook': 'www.facebook.com/sergiy.savarin',
-        'bio': 'I was born in small town in Ukrainian Carpatians in \
-                Lvivska region. There I finished school. Than I went \
-                to study to Lviv Ivan Franko National University. In \
-                2009 I finished the university and stayed in Lviv making \
-                a lot of diferent works. Life is awesome!!!'
-    }
-    return render(request, 'contact.html', {'owner': owner})
+    return render(request, 'contact.html')
