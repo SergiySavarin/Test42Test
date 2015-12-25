3b17d351a664cc4a4b0b88371ffe1aabf8019737
SergiySavarin <vir.host@gmail.com>
2015-12-25 15:15:04 +0200

    ticket:1 add test for contact model(saving and retriving data)

--- apps/contact/tests.py
+++ apps/contact/tests.py
@@ -1,8 +1,9 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
 
+from models import Owner
 from views import contact
 
 
@@ -19,3 +20,20 @@ class HomePageTest(TestCase):
         response = contact(request)
         expected_html = render_to_string('contact.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+
+class OwnerDataTest(TestCase):
+    """Test owner contact model."""
+    def test_saving_and_retriving_owner_data(self):
+        """Test saving and retriving owner data."""
+        owner = Owner()
+
+        owner.name = 'Sergiy'
+        owner.last_name = 'Savarin'
+        owner.save()
+
+        saved_data = Owner.objects.all()
+        self.assertEqual(saved_data.count(), 1)
+
+        self.assertEqual(saved_data.name, 'Sergiy')
+        self.assertEqual(saved_data.last_name, 'Savarin')
