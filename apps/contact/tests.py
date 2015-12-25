90837667f3274986fc5e65d371cd5a3576a9d687
SergiySavarin <vir.host@gmail.com>
2015-12-25 11:09:00 +0200

    ticket:1 tests hello and contact passes, add doc strings

--- apps/contact/tests.py
+++ apps/contact/tests.py
@@ -6,12 +6,14 @@ from django.template.loader import render_to_string
 from views import contact
 
 class HomePageTest(TestCase):
-
+    """Test contact home page."""
     def test_root_url_resolves_to_home_page_view(self):
+        """Test root url and home page url."""
         found = resolve('/')
         self.assertEqual(found.func, contact)
 
     def test_home_page_returns_correct_html(self):
+        """Test site and contact.html content."""
         request = HttpRequest()
         response = contact(request)
         expected_html = render_to_string('contact.html')
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def contact(request):
+    """View for contact.html root page."""
     return render(request, 'contact.html')
