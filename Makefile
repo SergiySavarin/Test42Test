dbe7a637080be1b31d84aeb9532b95fa43976919
SergiySavarin <vir.host@gmail.com>
2015-12-27 12:03:39 +0200

    ticket:1 add loaddata to makefile and owner fixture

--- Makefile
+++ Makefile
@@ -16,4 +16,7 @@ migrate:
 
 collectstatic:
 	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) collectstatic --noinput
+
+loaddata:
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$(SETTINGS) $(MANAGE) loaddata owner_data.json
 .PHONY: test syncdb migrate
--- apps/contact/views.py
+++ apps/contact/views.py
@@ -4,21 +4,5 @@ from .models import Owner
 
 def contact(request):
     """View for contact.html root page."""
-    owner = Owner()
-    # if owner have no data than create it
-    if len(Owner.objects.all()) == 0:
-        owner = Owner(
-            first_name='Sergiy', last_name='Savarin',
-            birthday='30/11/1986', email='vir.host@gmail.com',
-            skype='sergiy_savarin', jabber='sergiysavarin@khavr.com',
-            other='www.facebook.com/sergiy.savarin',
-            bio='I was born in small town in Ukrainian Carpatians \
-                in Lvivska region. There I finished school. \
-                Than I went to study to Lviv Ivan Franko National \
-                University. In 2009 I finished the university and \
-                stayed in Lviv making a lot of diferent works. \
-                Life is awesome!!!',
-        )
-        owner.save()
     owner = Owner.objects.all()[0]
     return render(request, 'contact.html', {'owner': owner})
