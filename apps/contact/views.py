3f11d1aaa6376aa134bb279d117419113b30a42b
SergiySavarin <vir.host@gmail.com>
2016-01-01 18:04:45 +0200

    ticket:3 update view code with ajax

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,22 +1,23 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Owner, UsersRequest
-import json
+
 
 def contact(request):
     """View for contact.html root page."""
+    # Take last ten requests from the database and sort its by id
     requests = UsersRequest.objects.order_by('id').reverse()[:10]
+    # Take owner data from the database
     owner = Owner.objects.all()[0]
+    # if request is ajax, prepare requests and
+    # send its in json format
     if request.is_ajax():
-        request_data = {'request': [user.request_str for user in requests]}
-        return HttpResponse(json.dumps(request_data), content_type='application/javascript')
+        request_data = {
+            'request': [('<p>%s</p>' % user.request_str) for user in requests]
+        }
+        return HttpResponse(json.dumps(request_data))
     else:
         return render(request, 'contact.html', {'owner': owner,
-                                            'requests': requests})
-
-def requests(request):
-    """for requests storing"""
-    requests = UsersRequest.objects.order_by('id').reverse()[:10]
-    request_data = {'request': [user.request_str for user in requests]}
-    # return HttpResponse(json.dumps(request_data), content_type='application/javascript')
-    return render(request, 'requests.html', {'requests': requests})
+                                                'requests': requests})
