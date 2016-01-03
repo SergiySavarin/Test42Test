89b6c2243126b21c9ea36e40e1177391e032d2c7
SergiySavarin <vir.host@gmail.com>
2016-01-03 10:39:59 +0200

    ticket:3 fix middlware to saving only http requests without ajax

--- apps/contact/middleware.py
+++ apps/contact/middleware.py
@@ -10,13 +10,14 @@ class UserRequestMiddleware(object):
                 Date       Time     Remote IP  Method Path Protocol
             [01/Jan/2016 13:01:27] [127.0.0.1]  "GET   /   HTTP/1.1"
         """
-        request_str = '[%s] [%s] "%s %s %s"' % (
-            datetime.now().strftime('%d/%b/%Y %H:%M:%S'),
-            request.META['REMOTE_ADDR'],
-            request.method,
-            request.path,
-            request.META['SERVER_PROTOCOL']
-        )
-        user_request = UsersRequest(request_str=request_str)
-        user_request.save()
+        if not request.is_ajax():    
+            request_str = '[%s] [%s] "%s %s %s"' % (
+                datetime.now().strftime('%d/%b/%Y %H:%M:%S'),
+                request.META['REMOTE_ADDR'],
+                request.method,
+                request.path,
+                request.META['SERVER_PROTOCOL']
+            )
+            user_request = UsersRequest(request_str=request_str)
+            user_request.save()
         return None
