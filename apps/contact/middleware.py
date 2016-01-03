7531a2c6b9e1e62454fe9bd4b4917518d74e0aa3
SergiySavarin <vir.host@gmail.com>
2016-01-03 11:11:50 +0200

    ticket:3 fix middleware to exclude static files requests

--- apps/contact/middleware.py
+++ apps/contact/middleware.py
@@ -9,15 +9,19 @@ class UserRequestMiddleware(object):
         """ All requests will be save to database in this format:
                 Date       Time     Remote IP  Method Path Protocol
             [01/Jan/2016 13:01:27] [127.0.0.1]  "GET   /   HTTP/1.1"
+            Exclusions:
+                - ajax requests;
+                - requests to static files;
         """
         if not request.is_ajax():
-            request_str = '[%s] [%s] "%s %s %s"' % (
-                datetime.now().strftime('%d/%b/%Y %H:%M:%S'),
-                request.META['REMOTE_ADDR'],
-                request.method,
-                request.path,
-                request.META['SERVER_PROTOCOL']
-            )
-            user_request = UsersRequest(request_str=request_str)
-            user_request.save()
+            if '/static/' not in request.path:
+                request_str = '[%s] [%s] "%s %s %s"' % (
+                    datetime.now().strftime('%d/%b/%Y %H:%M:%S'),
+                    request.META['REMOTE_ADDR'],
+                    request.method,
+                    request.path,
+                    request.META['SERVER_PROTOCOL']
+                )
+                user_request = UsersRequest(request_str=request_str)
+                user_request.save()
         return None
