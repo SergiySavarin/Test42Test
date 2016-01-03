4f2ba2348305590458f3f47d52b1f45eae51bb0e
SergiySavarin <vir.host@gmail.com>
2016-01-03 10:41:02 +0200

    ticket:3 add requests view

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -7,17 +7,24 @@ from .models import Owner, UsersRequest
 
 def contact(request):
     """View for contact.html root page."""
-    # Take last ten requests from the database and sort its by id
-    requests = UsersRequest.objects.order_by('id').reverse()[:10]
     # Take owner data from the database
     owner = Owner.objects.all()[0]
+    return render(request, 'contact.html', {'owner': owner})
+
+
+def requests(request):
+    """View for last ten requests to server."""
+    # Take last ten requests from the database and sort its by id
+    requests = UsersRequest.objects.order_by('id').reverse()[:10]
+    # Quantity of requests
+    count = UsersRequest.objects.count()
     # if request is ajax, prepare requests and
     # send its in json format
     if request.is_ajax():
         request_data = {
-            'request': [('<p>%s</p>' % user.request_str) for user in requests]
+            'request': [('<p>%s</p>' % user.request_str) for user in requests],
+            'count': count
         }
         return HttpResponse(json.dumps(request_data))
     else:
-        return render(request, 'contact.html', {'owner': owner,
-                                                'requests': requests})
+        return render(request, 'requests.html')
