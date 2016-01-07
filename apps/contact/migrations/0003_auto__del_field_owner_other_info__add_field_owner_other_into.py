746ef35fd584ebc65aafacd7c7093174686d93ae
SergiySavarin <vir.host@gmail.com>
2016-01-07 13:44:07 +0200

    ticket:5 add migration for model

---
+++ apps/contact/migrations/0003_auto__del_field_owner_other_info__add_field_owner_other_into.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Owner.other_info'
+        db.delete_column(u'contact_owner', 'other_info')
+
+        # Adding field 'Owner.other_into'
+        db.add_column(u'contact_owner', 'other_into',
+                      self.gf('django.db.models.fields.TextField')(default='', blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Adding field 'Owner.other_info'
+        db.add_column(u'contact_owner', 'other_info',
+                      self.gf('django.db.models.fields.TextField')(default='', blank=True),
+                      keep_default=False)
+
+        # Deleting field 'Owner.other_into'
+        db.delete_column(u'contact_owner', 'other_into')
+
+
+    models = {
+        u'contact.owner': {
+            'Meta': {'object_name': 'Owner'},
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'birthday': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'other_into': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'contact.usersrequest': {
+            'Meta': {'object_name': 'UsersRequest'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'request_str': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['contact']
