49348a0971317cb8f6493534421c992eb59a1e04
SergiySavarin <vir.host@gmail.com>
2016-01-07 23:59:03 +0200

    ticket:5 test add fake initial migration

---
+++ apps/contact/migrations/0001_initial 22.16.32.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        pass
+
+    def backwards(self, orm):
+        # Deleting model 'Owner'
+        db.delete_table(u'contact_owner')
+
+
+    models = {
+        u'contact.owner': {
+            'Meta': {'object_name': 'Owner'},
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'birthday': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'other': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['contact']
